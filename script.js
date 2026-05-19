040be1418ea129f77c0dc996e1b8a8cc61c93169
Nilsschaur <nils.schaur@angeskola.se>
2026-05-19 09:17:58 +0000

    [Add]-Navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {navMenu.classList.toggle("active");});
